638c8c4bcafbdd09a02e9b42f33aa35216885373
Simon Walker <s.r.walker101@googlemail.com>
2018-07-02 21:40:41 +0100

    Add web client wrapper

--- exploration.py
+++ exploration.py
@@ -4,33 +4,41 @@ import sqlite3
 import requests
 import functools
 
-def fetch_json(url):
-    url = update_url(url)
-    print(url)
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.json()
 
+class Client(object):
+    def __init__(self, verbose=False):
+        self.session = requests.Session()
+        self.verbose = verbose
 
-def update_url(old_url):
-    return old_url.replace('http://data', 'http://lda.data')
+    def fetch_json(self, url):
+        url = self.update_url(url)
+        if self.verbose:
+            print(url)
+        r = self.session.get(url)
+        r.raise_for_status()
+        return r.json()
+
+    @staticmethod
+    def update_url(old_url):
+        return old_url.replace('http://data', 'http://lda.data')
 
 
 page_size = 50
 
-entries = fetch_json(f'http://lda.data.parliament.uk/electionresults.json?_pageSize={page_size}')
+client = Client(verbose=True)
+entries = client.fetch_json(f'http://lda.data.parliament.uk/electionresults.json?_pageSize={page_size}')
 
 total_results = entries['result']['totalResults']
 n_requests = total_results // page_size + 1
 print(f'Making {n_requests} requests')
 
 for request_id in range(n_requests):
-    election_results = fetch_json(f'http://lda.data.parliament.uk/electionresults.json?_pageSize={page_size}&_page={request_id + 1}')
+    election_results = client.fetch_json(f'http://lda.data.parliament.uk/electionresults.json?_pageSize={page_size}&_page={request_id + 1}')
     entries = election_results['result']['items']
     for entry in entries:
         # Extract constituency info
         constituency_url = entry['constituency']['_about']
-        constituency_info = fetch_json(constituency_url + '.json')
+        constituency_info = client.fetch_json(constituency_url + '.json')
 
         constituency_type = constituency_info['result']['primaryTopic']['constituencyType']
         constituency_name = constituency_info['result']['primaryTopic']['label']['_value']
@@ -39,12 +47,12 @@ for request_id in range(n_requests):
         # Extract election info
 
         election_url = entry['_about']
-        election_info = fetch_json(election_url + '.json')
+        election_info = client.fetch_json(election_url + '.json')
         turnout = election_info['result']['primaryTopic']['turnout']
 
         # Extract detailed election info
         detailed_election_url = election_info['result']['primaryTopic']['election']['_about']
-        detailed_election_info = fetch_json(detailed_election_url + '.json')
+        detailed_election_info = client.fetch_json(detailed_election_url + '.json')
         election_type = detailed_election_info['result']['primaryTopic']['electionType']
         election_label = detailed_election_info['result']['primaryTopic']['label']['_value']
 
@@ -52,7 +60,7 @@ for request_id in range(n_requests):
         candidate_urls = election_info['result']['primaryTopic']['candidate']
 
         for candidate_url in candidate_urls:
-            candidate = fetch_json(candidate_url + '.json')
+            candidate = client.fetch_json(candidate_url + '.json')
 
             try:
                 vote_change_percentage = candidate['result']['primaryTopic']['voteChangePercentage']
